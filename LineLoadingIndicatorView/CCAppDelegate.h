0916d2238640fcf9a8e8f950c2a4f7fbc496d0cd
Nia Mutiara <nmutiara@gmail.com>
2013-07-15 17:06:59 +0800

    Initial Commit

---
+++ LineLoadingIndicatorView/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  LineLoadingIndicatorView
+//
+//  Created by Nia Mutiara on 15/7/13.
+//  Copyright (c) 2013 Cat Cyborg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LineLoadingIndicatorView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LineLoadingIndicatorView
+//
+//  Created by Nia Mutiara on 15/7/13.
+//  Copyright (c) 2013 Cat Cyborg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCAppDelegate class]));
+    }
+}
